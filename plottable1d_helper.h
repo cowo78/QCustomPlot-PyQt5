a7ea437e46e946a5130d1311ff79d4d740bf6ae6
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2019-07-18 12:24:54 +0200

    helper.h has been renamed to plottable1d_helper.h

--- plottable1d_helper.h
+++ plottable1d_helper.h
@@ -1,8 +1,8 @@
-#if !defined(__QCUSTOMPLOT_SIP_HELPER)
-#define __QCUSTOMPLOT_SIP_HELPER
+#if !defined(__QCUSTOMPLOT_SIP_PLOTTABLE1D_HELPER)
+#define __QCUSTOMPLOT_SIP_PLOTTABLE1D_HELPER
 
 typedef QCPAbstractPlottable1D<QCPGraphData> QCPAbstractPlottable1D_QCPGraphData;
 typedef QCPAbstractPlottable1D<QCPBarsData> QCPAbstractPlottable1D_QCPBarsData;
 typedef QCPAbstractPlottable1D<QCPCurveData> QCPAbstractPlottable1D_QCPCurveData;
 
-#endif  // __QCUSTOMPLOT_SIP_HELPER
+#endif  // __QCUSTOMPLOT_SIP_PLOTTABLE1D_HELPER
